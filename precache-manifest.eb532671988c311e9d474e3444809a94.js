05fb7385dcf7a99d631e80e408f449d334b00ff2
5teve5 <47039007+5teve5@users.noreply.github.com>
2019-09-29 23:10:33 +0100

    Updates

---
+++ precache-manifest.eb532671988c311e9d474e3444809a94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e7da33243138ff40cd3ccde975521c",
+    "url": "/Cocktails-app/index.html"
+  },
+  {
+    "revision": "ab88a09449d20403c575",
+    "url": "/Cocktails-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7f262b1524eac69ad269",
+    "url": "/Cocktails-app/static/css/main.47f12431.chunk.css"
+  },
+  {
+    "revision": "ab88a09449d20403c575",
+    "url": "/Cocktails-app/static/js/2.dd56b192.chunk.js"
+  },
+  {
+    "revision": "7f262b1524eac69ad269",
+    "url": "/Cocktails-app/static/js/main.b429d2c6.chunk.js"
+  },
+  {
+    "revision": "301985f24edca9e62338",
+    "url": "/Cocktails-app/static/js/runtime~main.d1cbc9d3.js"
+  },
+  {
+    "revision": "25de97e7f9350aeb3d765d941db692ef",
+    "url": "/Cocktails-app/static/media/cocktails-image.25de97e7.jpg"
+  }
+]);
